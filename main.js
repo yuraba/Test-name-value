c4a621a8ea5c1f54bb822759210a962a9a0aa5a7
starvinta <yurikulikov22@gmail.com>
2022-12-07 09:34:08 +0200

    m

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.querySelector(".button").onclick = (event) => {
 
         let myCheckBox = document.createElement('INPUT'); 
         myCheckBox.setAttribute("type", "checkbox");
+        myCheckBox.className = "checkbox";
         myLi.appendChild(myCheckBox);
         
         let mySpan = document.createElement('span'); 
@@ -28,11 +29,10 @@ document.querySelector(".button").onclick = (event) => {
         obj["selector"] = false;
         generalObject.push(obj);
         templateText = "";    
-        console.log(generalObject);
 }
 
 document.querySelector(".sortName").onclick = () => {
-        var geek_list, i, run, li, stop;
+        let geek_list, i, run, li, stop;
         geek_list = document.getElementById("GeekList");
         run = true;
         while (run) {
@@ -51,14 +51,62 @@ document.querySelector(".sortName").onclick = () => {
                         li[i + 1], li[i]);
                 run = true;
             }
-        }
-    
-        
+        }    
 }
 
-document.querySelector(".delete").onclick = () => {
-    
+document.querySelector(".sortValue").onclick = () => {
+    let ul = document.querySelector("#GeekList").value;
+    console.log(ul);
+    let geek_list, i, run, li, stop;
+    geek_list = document.getElementById("GeekList");
+    run = true;
+    let sp = []
+    while (run) {
+        run = false;
+        li = geek_list.getElementsByTagName("LI");
+        for (i = 0; i < (li.length - 1); i++) {
+            stop = false;
+            if (li[i].innerHTML.toLowerCase() > 
+                li[i + 1].innerHTML.toLowerCase()) {
+                stop = true;
+                break;
+            }
+            
+        }
+        if (stop) {
+            li[i].parentNode.insertBefore(
+                    li[i + 1], li[i]);
+            run = true;
+        }
+    }    
 }
+
+
+
+
+document.querySelector(".delete").onclick = () => {
+    let close = document.querySelectorAll("li");
+    console.log(close);
+        for (let i of close) {
+            if(document.querySelector(".checkbox").checked){
+                i.style.display = "none";
+            }
+        }
+        checkbox();
+}
+    
+function checkbox(){
+    let close = document.querySelectorAll("li");
+    for (let i of close) {
+        if(i.children.input.checkbox.checked){
+            generalObject.obj.selector = true;
+        }
+    }
+}
+    
+   
+
+
 
 
 
